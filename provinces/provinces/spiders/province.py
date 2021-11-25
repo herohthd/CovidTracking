542d14f512de3efeef1feccec1a15160a44db059
herohthd <herohthd@gmail.com>
2021-11-25 13:41:46 +0700

    crawl

---
+++ provinces/provinces/spiders/province.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy_splash import SplashRequest
+
+class ProvinceSpider(scrapy.Spider):
+    name = 'province'
+    allowed_domains = ['vnexpress.net/covid-19/vaccine']
+    start_urls = ['https://vnexpress.net/covid-19/vaccine']
+
+    def start_request(self):
+        url = "https://vnexpress.net/covid-19/vaccine"
+
+        yield SplashRequest(url=url,callback=self.parse)
+
+    def parse(self, response):
+        provinces = response.xpath("//div[@id='total_vaccine_province']/ul[@data-weight]")
+        for province in provinces:
+            yield{
+                'province_name':province.xpath(".//li[1]/text()").get(),
+                'province_population':province.xpath(".//li[2]/text()").get(),
+                'province_expected_distribution':province.xpath(".//li[3]/text()").get(),
+                'province_actual_distribution':province.xpath(".//li[4]/text()").get(),
+                'province_distribution_percentage':province.xpath(".//li[5]/text()").get(),
+            }
