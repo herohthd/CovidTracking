f2419ed0dd8bc8d2b7c699dcffd2a72d44b27229
huydq <herohthd@example.com>
2021-12-15 20:30:46 +0700

    Add graph

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
-
 import sys
 import gi
-import os
-import json
 
 gi.require_version('Gtk', '3.0')
 
--- src/window.py
+++ src/window.py
@@ -1,26 +1,48 @@
+#!/usr/bin/python3
 
 from gi.repository import Gtk
 from .vaccine import list
+
 
 @Gtk.Template(resource_path='/org/example/App/window.ui')
 class CovidtrackingWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'CovidtrackingWindow'
 
-    #label = Gtk.Template.Child()
+
+
     vaccinated_number = Gtk.Template.Child()
     vaccinated_percentage = Gtk.Template.Child()
     two_dose_number = Gtk.Template.Child()
     two_dose_percentage = Gtk.Template.Child()
     one_dose_number = Gtk.Template.Child()
     one_dose_percentage = Gtk.Template.Child()
+
+    enough_dose_number = Gtk.Template.Child()
+    progress_bar = Gtk.Template.Child()
+
+    top_vaccinated_provinces = Gtk.Template.Child()
+    least_vaccinated_provinces = Gtk.Template.Child()
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+
+
         self.vaccinated_number.set_text(list[0]["vaccinated_population"])
         self.vaccinated_percentage.set_text(list[0]["vaccinated_percentage"])
         self.two_dose_number.set_text(list[1]["two_dose_population"])
         self.two_dose_percentage.set_text(list[1]["two_dose_percentage"])
         self.one_dose_number.set_text(list[2]["one_dose_population"])
         self.one_dose_percentage.set_text(list[2]["one_dose_percentage"])
+
+
+        progress = float(list[1]["two_dose_percentage"].replace("% population",""))/100
+        self.enough_dose_number.set_text("Enough doses population: "+list[1]["two_dose_population"])
+        self.progress_bar.set_fraction(progress)
+
+
+
+
+
 
 
         
