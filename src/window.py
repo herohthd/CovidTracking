fdcb437765dc4126070e6c51c09755aa0f796abc
huydq <herohthd@example.com>
2021-12-17 12:36:34 +0700

    Add maps

--- src/window.py
+++ src/window.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+import gi
 
+gi.require_version("Gtk", "3.0")
+gi.require_version('WebKit2', '4.0')
 from gi.repository import Gtk
+from gi.repository import WebKit2
 from .vaccine import list
 
 
@@ -22,10 +26,11 @@ class CovidtrackingWindow(Gtk.ApplicationWindow):
 
     top_vaccinated_provinces = Gtk.Template.Child()
     least_vaccinated_provinces = Gtk.Template.Child()
+
+    distribution = Gtk.Template.Child()
+    vaccination = Gtk.Template.Child()
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-
 
         self.vaccinated_number.set_text(list[0]["vaccinated_population"])
         self.vaccinated_percentage.set_text(list[0]["vaccinated_percentage"])
@@ -38,6 +43,27 @@ class CovidtrackingWindow(Gtk.ApplicationWindow):
         progress = float(list[1]["two_dose_percentage"].replace("% population",""))/100
         self.enough_dose_number.set_text("Enough doses population: "+list[1]["two_dose_population"])
         self.progress_bar.set_fraction(progress)
+
+        self.top_vaccinated_provinces.set_from_file("/home/huydq/Projects/CovidTracking/src/images/top_vaccinated_provinces.png")
+        self.least_vaccinated_provinces.set_from_file("/home/huydq/Projects/CovidTracking/src/images/least_vaccinated_provinces.png")
+
+        self.webview = WebKit2.WebView()
+        uri = "https://api.ncovtrack.com/vaccine/vietnam/provinces?metric=doses_available&showTable=false&showMap=true"
+        self.webview.load_uri(uri)
+        self.distribution.pack_start(self.webview,True,True,0)
+
+        self.webview1 = WebKit2.WebView()
+        uri1 = "https://api.ncovtrack.com/vaccine/vietnam/provinces?metric=second_dose&showTable=false&showMap=true"
+        self.webview1.load_uri(uri1)
+        self.vaccination.pack_start(self.webview1,True,True,0)
+
+        self.show_all()
+
+
+
+
+
+
 
 
 
